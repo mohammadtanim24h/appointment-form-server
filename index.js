e35dc55b065b11416e9a1b828e469f877c99c949
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-06-05 14:00:04 +0600

    connected to db successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,13 +9,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-    "mongodb+srv://appointmentAdmin:<password>@cluster0.nqt3zjy.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nqt3zjy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("db connected");
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Appointment Form server is up and running");
