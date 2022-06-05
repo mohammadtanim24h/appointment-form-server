60006ac2f2f31a1e20380af5198d16b5ad266013
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-06-05 13:52:40 +0600

    mongodb uri, client and middlewares added

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+    "mongodb+srv://appointmentAdmin:<password>@cluster0.nqt3zjy.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", (req, res) => {
+    res.send("Appointment Form server is up and running");
+});
+
+app.listen(port, () => {
+    console.log("Listening to appointment form, port", port);
+});
